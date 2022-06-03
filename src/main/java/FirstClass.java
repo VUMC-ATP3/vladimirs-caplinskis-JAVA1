290e565f9f0cea87fa0924ac9d51c04ebe649099
vladimirs-caplinskis <104638042+vladimirs-caplinskis@users.noreply.github.com>
2022-06-03 20:00:32 +0300

    Initial commit

---
+++ src/main/java/FirstClass.java
@@ -0,0 +1,119 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+        //1.uzdevums
+
+        String valsts = "Latvija";
+        double population = 1.902;
+        float platiba = 64.589F;
+        String capital = "Rīga";
+        String valoda = "Latviešu";
+        boolean isInEu = true;
+        char valuta = '€';
+
+        System.out.println("Valsts");
+        System.out.println(valsts);
+        System.out.println("Iedzīvotājus skaits");
+        System.out.println(population);
+        System.out.println("Galvaspilsēta");
+        System.out.println(capital);
+        System.out.println("Oficiāla valoda");
+        System.out.println(valoda);
+        System.out.println("Vai ir ES dalībvalsts");
+        System.out.println(isInEu);
+        System.out.println("Valūta");
+        System.out.println(valuta);
+        System.out.println();
+        System.out.println();
+
+
+
+
+
+
+        //2.uzdevums
+        //Vērtēšanas sistēma
+        //Aptuvenā  atzīme  ballu -10 ballu skala skolēnu izglītības sasniegumu vērtēšanai:
+        //A- 10,9 Augsts (produktīvs radošais)
+        // B- 8,7 Pietiekami (produktīvi)
+        //C- 6,5 Vidējs (reproduktīvs-produktīvs)
+        //D- 4,3 Apmierinošs (uztverošs-produktīvs)
+        //E- 2,1  Zems (uztverošs)
+        //F- 0 Nav atbildes vai nav atbildes
+        int ballu;
+        int A = 10;
+        int B = 8;
+        int C = 6;
+        int D = 4;
+        int E = 2;
+        int F = 0;
+
+
+        // (  ) -Ielikt burtus
+
+        ballu = ( C );
+
+        System.out.println("Apguves līmenis");
+        System.out.println( ballu );
+
+        System.out.println();
+
+
+
+
+
+
+
+
+
+
+        //3.uzdevums
+
+        double summa;
+        int a = 24;
+        int b = 30;
+        double c = 24;
+        double d = 30;
+
+        summa = a + b;
+        System.out.println("Saskaitīšana");
+        System.out.println(summa);
+
+        System.out.println();
+
+        summa = a - b;
+        System.out.println("Atņemšana");
+        System.out.println(summa);
+
+        System.out.println();
+
+        summa = a * b;
+        System.out.println("Reizināšana");
+        System.out.println(summa);
+
+        System.out.println();
+
+        summa = c / d;
+        System.out.println("Dalīšana");
+        System.out.println(summa);
+
+        System.out.println(  );
+
+        summa = a % b;
+        System.out.println("Atlikums");
+        System.out.println(summa);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
